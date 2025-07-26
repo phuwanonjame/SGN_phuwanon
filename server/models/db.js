9230d5450ff2f1d4a951934a969b6c2c3aa9c615
PK1ST <148850215+phuwanonjame@users.noreply.github.com>
2025-07-26 21:27:00 +0700

    Update db.js

--- server/models/db.js
+++ server/models/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Samart@db123',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'login_db',
   waitForConnections: true,
   connectionLimit: 10,
